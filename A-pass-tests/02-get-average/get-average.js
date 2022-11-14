e3369acaf283d2247f0ebadadee67fb4a9830eac
Petar Nikolov <nikolov@Petars-MBP.broadband>
2022-11-14 08:14:18 +0000

    -

--- A-pass-tests/02-get-average/get-average.js
+++ A-pass-tests/02-get-average/get-average.js
@@ -2,6 +2,11 @@
 // return the average of all the numbers
 // be sure to exclude the strings
 
-function average(numbers) {}
-
+function average(numbers) {
+    let result = numbers.filter((element) => typeof element === 'number')
+    console.log(result)
+    let average = result.reduce((a, b) => a + b) / result.length
+    return average
+}
+console.log(average([13, 10, 12, "i", "a"]));
 module.exports = average;
--- B-write-tests/03-remove-vowels-from-array/remove-vowels-in-array.test.js
+++ B-write-tests/03-remove-vowels-from-array/remove-vowels-in-array.test.js
@@ -2,8 +2,14 @@ let removeVowelsFromWords = require("./remove-vowels-in-array");
 
 test("remove vowels from all words in array", function () {
   // Arrange
+  let words = ["irina", "etza", "daniel"];
+
   // Act
+  let result = removeVowelsFromWords(words);
+
   // Assert
+  let expected = ["rn", "tz", "dnl"];
+  expect(result).toEqual(expected);
 });
 
 // example
